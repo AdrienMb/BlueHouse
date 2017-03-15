1b8dc96dd85d01e6f5d596b6d0f6e68f141ddfbb
AdrienMb <adrien.mbirko@gmail.com>
2017-03-15 11:15:24 +0100

    Version 0.1

--- index.php
+++ index.php
@@ -1,16 +1,21 @@
 <!DOCTYPE html>
 <meta charset="UTF-8">
+<LINK href="style.css" rel="stylesheet" type="text/css">
 <html>
 <head>
 <title>Page d'accueil</title>
 </head>
-<body>
+<header>
 <img src="logo-app2.png">
+</header>
+<body>
 
+<form>
+<div class="formulaire1">
 <p>
     On insèrera ici les éléments de notre formulaire.
 </p>
-<input type="text" name="prenom" />
+<input type="text" name="prenom" placeholder="Mets ton nom bitch !" />
 
   <input type="submit" value="Valider" />
   <p>Avez vous une installation BlueHouse ?</p>
@@ -18,8 +23,9 @@
 <input type="radio" name="installation" value="oui" id="oui" checked="checked" /> <label for="oui">Oui</label>
 
 <input type="radio" name="installation" value="non" id="non" /> <label for="non">Non</label>
-<script type="text/javascript">alert('Tg William')</script>
 
+<script type="text/javascript">alert('Tg William')</script>
+</div>
 </form>
 </body>
 </html>
