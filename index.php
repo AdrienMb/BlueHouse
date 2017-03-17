4fa98fd33a348902e299cbf98c21769baf90789a
wildum <helmutlbb@orange.fr>
2017-03-17 11:47:47 +0100

    test bdd
    ko

--- index.php
+++ index.php
@@ -3,35 +3,39 @@
 <LINK href="style.css" rel="stylesheet" type="text/css">
 <html>
 <head>
-<title>Page d'accueil</title>
+  <title>Page d'accueil</title>
 </head>
 <header>
-<img src="images/logo-app2.png" class="logo">
+  <img src="images/logo-app2.png" class="logo">
 </header>
 <body>
+  <p>
+    <?php
+    try
+      {
+        $bdd = new PDO('mysql:host=localhost;dbname=minichat;charset=utf8','root','');
+      }
+      catch(Exception $e)
+      {
+        die('Erreur :'.$e->getMessage());
+      }
+      ?>
+  </p>
+  <form>
+  <div class="formulaire1">
+    <h2>Inscrivez vous !</h2>
+    <p>
+        On insèrera ici les éléments de notre formulaire.
+    </p>
+      <input type="text" name="prenom" placeholder="Mets ton nom !" />
+      <input type="submit" value="Valider" />
+    <p>Avez vous une installation BlueHouse ?</p>
+    <input type="radio" name="installation" value="oui" id="oui" checked="checked" /> <label for="oui">Oui</label>
 
-<form>
-<div class="formulaire1">
-  <h2>Inscrivez vous !</h2>
-<p>
-    On insèrera ici les éléments de notre formulaire.
-</p>
-<input type="text" name="prenom" placeholder="Mets ton nom !" />
+    <input type="radio" name="installation" value="non" id="non" /> <label for="non">Non</label>
 
-  <input type="submit" value="Valider" />
-  <p>Avez vous une installation BlueHouse ?</p>
-
-<input type="radio" name="installation" value="oui" id="oui" checked="checked" /> <label for="oui">Oui</label>
-
-<input type="radio" name="installation" value="non" id="non" /> <label for="non">Non</label>
-
-<script type="text/javascript">alert('Tg William')</script>
-</div>
+    <script type="text/javascript">alert('Tg William')</script>
+  </div>
 </form>
 </body>
-<footer>
-
-
-</footer>
-
 </html>
