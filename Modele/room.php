a93cb0dcff7ddbabce6363fa660ef1f8efb35b62
AdrienMb <adrien.mbirko@gmail.com>
2017-05-15 11:52:46 +0200

    travaux gestion des capteurs

--- Modele/room.php
+++ Modele/room.php
@@ -1,7 +1,6 @@
 <?php
-    require("connexion.php");
 
-
+require($_SERVER["DOCUMENT_ROOT"]."/github/bluehouse/modele/connexion.php");
     function get_rooms($bdd,$id_house){
         $reponse = $bdd->query('SELECT name FROM room WHERE idHouse="'.$id_house.'"');
         return $reponse;
--- Modele/user.php
+++ Modele/user.php
@@ -1,24 +1,24 @@
 <?php
-    require("connexion.php");
 
+require($_SERVER["DOCUMENT_ROOT"]."/github/bluehouse/modele/connexion.php");
 
-    function get_idHouse($bdd,$email){
-        $reponse = $bdd->query('SELECT idHouse FROM user WHERE email="'.$email.'"');
+    function get_idHouse($bdd,$id){
+        $reponse = $bdd->query('SELECT idHouse FROM user WHERE id="'.$id.'"');
         return $reponse;
     }
 
-    function get_nameSexe($bdd,$email){
-        $reponse = $bdd->query('SELECT firstName,lastName,sexe FROM user WHERE email="'.$email.'"');
+    function get_nameSexe($bdd,$id){
+        $reponse = $bdd->query('SELECT firstName,lastName,sexe FROM user WHERE id="'.$id.'"');
         return $reponse;
     }
 
-    function get_phoneNumber($bdd,$email){
-        $reponse = $bdd->query('SELECT phoneNumber FROM user WHERE email="'.$email.'"');
+    function get_phoneNumber($bdd,$id){
+        $reponse = $bdd->query('SELECT phoneNumber FROM user WHERE id="'.$id.'"');
         return $reponse;
     }
 
-    function get_id($bdd,$email){
-        $reponse = $bdd->query('SELECT id FROM user WHERE email="'.$email.'"');
+    function get_id($bdd,$id){
+        $reponse = $bdd->query('SELECT id FROM user WHERE id="'.$id.'"');
         return $reponse;
     }
 
