dd691bbe69a6801f2942e09a710e90332297fb7f
wildum <helmutlbb@orange.fr>
2017-05-17 11:55:06 +0200

    dvp page stats
    - dvp page stats

--- Controleur/statistiques.php
+++ Controleur/statistiques.php
@@ -5,6 +5,7 @@ require($_SERVER["DOCUMENT_ROOT"].'/github/bluehouse/Vue/header.php');
 require($_SERVER["DOCUMENT_ROOT"].'/github/bluehouse/Vue/footer.php');
 require($_SERVER["DOCUMENT_ROOT"]."/github/bluehouse/modele/room.php");
 session_start();
+include($_SERVER["DOCUMENT_ROOT"]."/github/bluehouse/modele/dataSensor.php");
 $id = $_SESSION['userID'];
 $rep=get_idHouse($bdd,$id);
 $idh = $rep->fetch();
@@ -26,9 +27,19 @@ while($result = $test->fetch(PDO::FETCH_ASSOC))
 ?>
 </ul>
 <?php
-explode(" ",$sensors['idSensors']);
 if(isset($_GET['nbRoom'])){
-  echo $_GET['nbRoom'];
+  $piece_selected = $pieces[$_GET['nbRoom']];
+  $sensors_selected = $tab_sensors[$_GET['nbRoom']];
+  $tab_sensors_selected = explode(" ",$sensors_selected);
+  foreach ($tab_sensors_selected as $value){
+    $rep3 = get_dataWithDate($bdd,$value);
+    $j = 0;
+    while(($data = $rep3->fetch(PDO::FETCH_ASSOC)) && ($j < 20))
+    {
+      $j=$j+1;
+      echo $data['data'] .' '.$data['receptionDate'].' ' .'<br />';
+    }
+}
 }
 
  ?>
---
+++ Vue/graphe.php
@@ -0,0 +1,16 @@
+&lt;script language="javascript" type="text/javascript" src="../jquery.js"&gt;&lt;/script&gt;
+&lt;script language="javascript" type="text/javascript" src="../jquery.flot.js"&gt;&lt;/script&gt;
+&lt;script language="javascript" type="text/javascript" src="../jquery.flot.navigate.js"&gt;&lt;/script&gt;
+&lt;script language="javascript" type="text/javascript" src="../jquery.flot.selection.js"&gt;&lt;/script&gt;
+&lt;script language="javascript" type="text/javascript" src="../jquery.flot.crosshair.js"&gt;&lt;/script&gt;
+
+&lt;div id="page"&gt;
+	&lt;div id="miniature"&gt;
+		&lt;p id="overviewLegend"&gt;&lt;/p&gt;
+	&lt;/div&gt;
+	&lt;p id="hoverdata"&gt;&lt;/p&gt;
+&lt;/div&gt;
+
+<script>
+var donnees = [ [30, 0], [40, 1], [50, 2] ];
+</script>
