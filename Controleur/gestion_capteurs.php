e854138d250d0fe89b9caf042125bf80a85a8186
wildum <helmutlbb@orange.fr>
2017-05-31 11:59:23 +0200

    bouton supprimer pour les rooms
    - mise en place du bouton supprimer pour les rooms

--- Controleur/gestion_capteurs.php
+++ Controleur/gestion_capteurs.php
@@ -13,18 +13,29 @@ require($_SERVER["DOCUMENT_ROOT"]."/github/bluehouse/modele/room.php");
         while($result = $test->fetch(PDO::FETCH_ASSOC))
         {
           $pieces[] =  $result['name'];
+          $id_pieces[] = $result['id'];
           $i = $i+1;
         }
         require($_SERVER["DOCUMENT_ROOT"]."/github/bluehouse/Vue/intro_gestion_capteur.php");
         for($i; $i>=0; $i--)
         {
         	$nomPiece = $pieces[$i];
+          $idpiece = $id_pieces[$i];
           require($_SERVER["DOCUMENT_ROOT"]."/github/bluehouse/Vue/gestion_capteurs.php");
         }
         require($_SERVER["DOCUMENT_ROOT"]."/github/bluehouse/Vue/formulaire_piece.php");
         if(!empty($_POST['nomPiece'])){
           $rep=set_room($bdd,$_POST['nomPiece'],$idh['idHouse'])->fetch();
           header('Location: gestion_capteurs.php');
+        }
+
+        if(!empty($_POST['delete'])){
+          delete_room($bdd,$_POST['delete']);
+          echo '<script language="Javascript">
+        <!--
+        document.location.replace("gestion_capteurs.php");
+        // -->
+        </script>';
         }
 
 include($_SERVER["DOCUMENT_ROOT"]."/github/bluehouse/Vue/fin_gestion_capteur.php");
--- Vue/gestion_capteurs.php
+++ Vue/gestion_capteurs.php
@@ -1,7 +1,14 @@
 <title>Gestion des capteurs</title>
 
 <div class="pieces">
+  <form method="post" action="/github/bluehouse/Controleur/gestion_capteurs.php">
 <?php
-print ("<a href ='/github/bluehouse/controleur/salle.php?idsalle=$nomPiece'>".$nomPiece."</a><br/>");
+echo "<a href =/github/bluehouse/controleur/salle.php?idsalle=".$nomPiece.">".$nomPiece."</a>";
 ?>
+
+
+<div class="bton">
+<input type="image" name="delete" src="/github/bluehouse/Images/PETITE_POUBELLE.png" value=<?php echo $idpiece; ?> />
+</div>
+</form>
 </div>
