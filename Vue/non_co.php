3c8a5e236c8572f1195cd0a5c8f7af5c2e7fac50
AdrienMb <adrien.mbirko@gmail.com>
2017-06-09 10:51:59 +0200

    Push pour pull

---
+++ Vue/non_co.php
@@ -0,0 +1,10 @@
+Connectez-vous pour accéder à cette page !
+<?php
+if($_SESSION==null){
+require($_SERVER["DOCUMENT_ROOT"].'/github/bluehouse/Vue/header_deco.php');
+require($_SERVER["DOCUMENT_ROOT"].'/github/bluehouse/Vue/non_co.php');
+require($_SERVER["DOCUMENT_ROOT"].'/github/bluehouse/Vue/footer.php');
+}
+else {
+
+}
